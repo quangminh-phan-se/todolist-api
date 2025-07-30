87a9f43150f4d73041ffd2d9393bb49dc3095fe4
minh.phan <quangminh.phan.se@gmail.com>
2025-07-30 16:08:16 +0700

    chore: setup database

---
+++ src/database/migrations/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
